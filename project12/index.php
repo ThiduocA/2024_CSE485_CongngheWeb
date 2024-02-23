db8b4c2a6c74d770128b2bcd26a080e10421a391
NguyenHoang1234 <nguyenhoang02102003@gmail.com>
2024-02-23 15:03:44 +0700

    update project12

--- project12/index.php
+++ project12/index.php
@@ -1,11 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   gaubaccuc code
-</body>
-</html>
+
+<?php
+include "header.php";
+
+    $naviteam = [
+        "GIOI THIEU",
+        "TIN TUC & THONG BAO",
+        "DAO TAO",
+        "TUYEN SINH",
+        "NGHIEN CUU",
+        "DOI NGOAI",
+        "VAN BAN",
+        "SINH VIEN",
+        "LIEN HE"
+    ];
+    echo '<div class="header-bridcrum container"><nav><ul class="bridcum-list">';
+
+                    foreach ($naviteam as $item){
+                     echo    "<li class='bridcrum-item'>
+                                    <a class='bridcrum-link' href=''>$item</a>
+                             </li>";
+                    }
+    echo  '</ul></nav></div>';
+    
+      
+?>
+
+
+
