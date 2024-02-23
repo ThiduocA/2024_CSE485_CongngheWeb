5788779df0da530342d353bec4be17f1fab42abb
Hwngne <Xhung2k3@gmail.com>
2024-02-23 10:46:14 +0700

    Update project12

--- project12/index.php
+++ project12/index.php
@@ -1,1 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+<?php
+$navItems = [
+    "GIỚI THIỆU",
+    "TIN TỨC & THÔNG BÁO",
+    "TUYỂN SINH",
+    "ĐÀO TẠO",
+    "NGHIÊN CỨU",
+    "ĐỐI NGOẠI",
+    "VĂN BẢN",
+    "SINH VIÊN",
+    "LIÊN HỆ"
+];
+echo '<nav><ul class="navigation">';
+echo '<a href=""> <li class="fixDot"> icon </li> </a>';
+foreach ($navItems as $item) {
+    echo "<a href='' <li class='fixDot'>$item</li> </a>";
+}
+echo '</ul></nav>';
+?>
