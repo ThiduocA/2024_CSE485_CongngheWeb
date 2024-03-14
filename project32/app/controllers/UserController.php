7d21f7a73881889d872d98a402612b396e64d598
Hwngne <Xhung2k3@gmail.com>
2024-03-14 21:55:12 +0700

    update project

--- project32/app/controllers/UserController.php
+++ project32/app/controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
-//include(ROOT . 'services/UserService.php');
-include(ROOT . "/services/EmployeeService.php");
+require_once(ROOT . "/services/EmployeeService.php");
+require_once(ROOT . '/services/UserService.php');
+
 class UserController
 {
     public function index()
@@ -12,8 +13,42 @@ class UserController
         include('views/users/index.php');
     }
 
-    public function logout(){
+    public function logout()
+    {
         session_destroy();
         header('Location:?controller=guest&action=login');
+    }
+
+    public function info()
+    {
+        //header("Location: ?controller=user&action=info1");
+        include(ROOT . '/views/information-page/info.php');
+    }
+
+
+    public function edit_info()
+    {
+        //header("Location: ?controller=user&action=edit1");
+        include(ROOT . '/views/information-page/edit_info.php');
+    }
+
+    public function process_edit_info()
+    {
+        include(ROOT . '/views/information-page/process_edit_info.php');
+    }
+
+    public function change_password()
+    {
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+        }
+        $userService = new UserService();
+        $user = $userService->getUserById1($id);
+
+        include(ROOT . '/views/information-page/change_password.php');
+    }
+    public function process_change_password()
+    {
+        include(ROOT . '/views/information-page/process_change_password.php');
     }
 }
